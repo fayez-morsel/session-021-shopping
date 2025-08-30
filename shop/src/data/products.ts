b869f4bb2ad390b82c65fd27c957ec787c4c6d29
Fayez Morsel <fayezmorsel77@gmail.com>
2025-08-31 01:08:03 +0300

    add some photo and item

--- shop/src/data/products.ts
+++ shop/src/data/products.ts
@@ -1,10 +1,10 @@
 import type { Product } from "../types";
 
 export const products: Product[] = [
-  { id: 1, name: "Laptop", price: 999, image: "/images/laptop.png" },
-  { id: 2, name: "Smartphone", price: 499, image: "/images/phone.png" },
-  { id: 3, name: "Smartphone", price: 499, image: "/images/phone.png" },
-  { id: 4, name: "Smartphone", price: 499, image: "/images/phone.png" },
-  { id: 5, name: "Smartphone", price: 499, image: "/images/phone.png" },
-  { id: 6, name: "Smartphone", price: 499, image: "/images/phone.png" },
+  { id: 1, name: "Mac M1", price: 999, image: "/images/laptop.png" },
+  { id: 2, name: "Asus ROG", price: 1300, image: "/images/asus.png" },
+  { id: 3, name: "GeForce RTX 5090", price: 3000, image: "/images/5090.png" },
+  { id: 4, name: "HP Portable 17", price: 499, image: "/images/hp.png" },
+  { id: 5, name: "Lenovo IdeaPad Gaming 3", price: 1050, image: "/images/lenovo.png" },
+  { id: 6, name: "Dell Latitude", price: 399, image: "/images/dell.png" },
 ];
